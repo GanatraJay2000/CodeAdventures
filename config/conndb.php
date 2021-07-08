2a391ef5281629c73e7ba1ab3c9f4da6fcd3459d
Jay Ganatra <jbg@somaiya.edu>
2021-07-08 12:41:04 +0530

    removed foreign key constraints

--- config/conndb.php
+++ config/conndb.php
@@ -52,7 +52,7 @@ $create = $conn->query("CREATE TABLE IF NOT EXISTS regions(
     detailed_address varchar(255),
     main_branch_id INT(6),
     phone_no BIGINT(10) NOT NULL UNIQUE,
-    FOREIGN KEY(`zone_id`) REFERENCES zones(`id`) ON DELETE CASCADE,
+    -- FOREIGN KEY(`zone_id`) REFERENCES zones(`id`) ON DELETE CASCADE,
     CONSTRAINT unique_name UNIQUE (name, zone_id)
 )");
 if (!$create) {
@@ -69,7 +69,7 @@ $create = $conn->query("CREATE TABLE IF NOT EXISTS branches(
     town varchar(255),
     city varchar(255),
     region_id INT(6) NOT NULL,
-    FOREIGN KEY(`region_id`) REFERENCES regions(`id`) ON DELETE CASCADE,
+    -- FOREIGN KEY(`region_id`) REFERENCES regions(`id`) ON DELETE CASCADE,
     CONSTRAINT unique_name UNIQUE (name, region_id) 
 )");
 if (!$create) {
@@ -85,7 +85,7 @@ $create = $conn->query("CREATE TABLE IF NOT EXISTS hubs(
     town varchar(255),
     city varchar(255),
     branch_id INT(6) NOT NULL,
-    FOREIGN KEY(`branch_id`) REFERENCES branches(`id`) ON DELETE CASCADE,
+    -- FOREIGN KEY(`branch_id`) REFERENCES branches(`id`) ON DELETE CASCADE,
     CONSTRAINT unique_name UNIQUE (name, branch_id)
 )");
 if (!$create) {
@@ -101,7 +101,7 @@ $create = $conn->query("CREATE TABLE IF NOT EXISTS sites(
     town varchar(255),
     city varchar(255),
     hub_id INT(6) NOT NULL,
-    FOREIGN KEY(`hub_id`) REFERENCES hubs(`id`) ON DELETE CASCADE,
+    -- FOREIGN KEY(`hub_id`) REFERENCES hubs(`id`) ON DELETE CASCADE,
     CONSTRAINT unique_name UNIQUE (name, hub_id)
 )");
 if (!$create) {
@@ -122,7 +122,7 @@ $create = $conn->query("CREATE TABLE IF NOT EXISTS vendors(
     phone_no BIGINT(10) NOT NULL UNIQUE,
     no_of_employees INT(6) NOT NULL,
     region_id INT(6) NOT NULL,
-    FOREIGN KEY(`region_id`) REFERENCES regions(`id`) ON DELETE CASCADE,
+    -- FOREIGN KEY(`region_id`) REFERENCES regions(`id`) ON DELETE CASCADE,
     CONSTRAINT unique_name UNIQUE (name, region_id)
 )");
 if (!$create) {
@@ -146,9 +146,9 @@ $create = $conn->query("CREATE TABLE IF NOT EXISTS employees(
     extra_hours_amt BIGINT(10) UNSIGNED,
     base_amt BIGINT(10) UNSIGNED,
     no_of_working_sundays INT(6),
-    sunday_working_amt BIGINT(10) UNSIGNED,
-    FOREIGN KEY(`hub_id`) REFERENCES hubs(`id`) ON DELETE CASCADE,
-    FOREIGN KEY(`vendor_id`) REFERENCES vendors(`id`) ON DELETE CASCADE
+    sunday_working_amt BIGINT(10) UNSIGNED
+    -- FOREIGN KEY(`hub_id`) REFERENCES hubs(`id`) ON DELETE CASCADE,
+    -- FOREIGN KEY(`vendor_id`) REFERENCES vendors(`id`) ON DELETE CASCADE
 )");
 if (!$create) {
     print_r($conn->error);
@@ -162,9 +162,9 @@ $create = $conn->query("CREATE TABLE IF NOT EXISTS vehicles(
     vehicle_type varchar(255) NOT NULL,
     details text,
     vendor_id INT(6) NOT NULL,
-    hub_id INT(6) NOT NULL,
-    FOREIGN KEY(`vendor_id`) REFERENCES vendors(`id`) ON DELETE CASCADE,
-    FOREIGN KEY(`hub_id`) REFERENCES hubs(`id`) ON DELETE CASCADE
+    hub_id INT(6) NOT NULL
+    -- FOREIGN KEY(`vendor_id`) REFERENCES vendors(`id`) ON DELETE CASCADE,
+    -- FOREIGN KEY(`hub_id`) REFERENCES hubs(`id`) ON DELETE CASCADE
 )");
 if (!$create) {
     print_r($conn->error);
@@ -182,10 +182,10 @@ $create = $conn->query("CREATE TABLE IF NOT EXISTS transactions(
     amt_transfered BIGINT(10) UNSIGNED,
     vehicle_start_km varchar(255),
     vehicle_end_km varchar(255),
-    total_time_taken varchar(255),
-    FOREIGN KEY(`emp_id`) REFERENCES employees(`id`) ON DELETE CASCADE,
-    FOREIGN KEY(`vehicle_id`) REFERENCES vehicles(`id`) ON DELETE CASCADE,
-    FOREIGN KEY(`site_id`) REFERENCES sites(`id`) ON DELETE CASCADE
+    total_time_taken varchar(255)
+    -- FOREIGN KEY(`emp_id`) REFERENCES employees(`id`) ON DELETE CASCADE,
+    -- FOREIGN KEY(`vehicle_id`) REFERENCES vehicles(`id`) ON DELETE CASCADE,
+    -- FOREIGN KEY(`site_id`) REFERENCES sites(`id`) ON DELETE CASCADE
 )");
 if (!$create) {
     print_r($conn->error);
