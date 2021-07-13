57d8e501973a6f6f3c4aa343ad255b86a14a3754
Jay Ganatra <jbg@somaiya.edu>
2021-07-13 23:16:35 +0530

    changes

---
+++ portal/qr/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QR Code</title>
+</head>
+
+<body>
+    <img src="./generate.php?id=4" style="border:5px solid;width:100%" />
+</body>
+
+</html>
